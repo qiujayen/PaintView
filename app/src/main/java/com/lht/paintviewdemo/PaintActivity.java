a6fe1fa1e248f22f4effd22f5c9daa551d2b02af
lht <lht91225@gmail.com>
2017-09-30 16:22:21 +0800

    feat: 状态栏颜色

--- app/src/main/java/com/lht/paintviewdemo/PaintActivity.java
+++ app/src/main/java/com/lht/paintviewdemo/PaintActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
